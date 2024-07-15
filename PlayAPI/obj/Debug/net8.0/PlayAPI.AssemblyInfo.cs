fc7334822950beb4448d9b3698195f01f700ca5e
Katarina <katarinaandrejevic93@gmail.com>
2024-07-15 22:05:14 +0200

    Initial commit

--- PlayAPI/obj/Debug/net8.0/PlayAPI.AssemblyInfo.cs
+++ PlayAPI/obj/Debug/net8.0/PlayAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e041da303e8f71de5761a4d53ec4ff11931727")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestUI/obj/Debug/net8.0/TestUI.AssemblyInfo.cs
+++ TestUI/obj/Debug/net8.0/TestUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e041da303e8f71de5761a4d53ec4ff11931727")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
